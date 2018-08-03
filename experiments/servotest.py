01c8700c52a51ca2abfc77d5c6b524c95bec6fa4
Jonathan Sanderson <lists@quernstone.com>
2018-08-03 15:24:11 +0100

    Servo test

---
+++ experiments/servotest.py
@@ -0,0 +1,21 @@
+from gpiozero import Device, servo
+from gpiozero.pins.pigpio import PiGPIOFactory
+from time import sleep
+
+Device.pin_factory = PiGPIOFactory()
+
+myservo = [Servo(27), Servo(22), Servo(5), Servo(6),
+           Servo(13), Servo(19), Servo(26), Servo(21)]
+
+for servo in myservo:
+    myservo[servo].min()
+    sleep(0.1)
+
+sleep(1)
+
+for servo in myservo:
+    myservo[servo].mid()
+    sleep(0.1)
+    myservo[servo].min()
+    sleep(0.1)
+
